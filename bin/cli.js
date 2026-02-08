6531b89438e69ff3eb405bbf3f9f1e2ef64174ad
Piyush <classic.piyush@gamil.com>
2026-02-08 23:08:04 +0530

    added cli.js file

--- bin/cli.js
+++ bin/cli.js
@@ -1,17 +1,1 @@
 #!/usr/bin/env node
-
-const { parseFile } = require("../src/index");
-
-function showUsage() {
-  console.log("Usage: js-dependency-tracer <entry-file>");
-  console.log("Example: js-dependency-tracer src/index.ts");
-}
-
-const entry = process.argv[2];
-
-if (!entry) {
-  showUsage();
-  process.exit(1);
-}
-
-console.log(JSON.stringify(parseFile(entry), null, 2));
